d8821c314721a021aa6323a1276b69d696018e97
Snehapandey04 <pandeysneha.2560@gmail.com>
2025-07-03 22:25:28 +0530

    Use cloudinary

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,3 +1,4 @@
+import cloudinary from "../lib/cloudinary.js";
 import { generateToken } from "../lib/utils.js";
 import User from "../models/user.js";
 // Function for signup
@@ -66,4 +67,26 @@ export const login = async () => {
 
 export default checkAuth = (req,res) =>{
   res.json({success: true, user: req.user});
+}
+
+//Controller to update user profile details
+
+export const updateProfile = async(req,res) => {
+  try {
+    const{profilePic, bio, fullName} = req.body;
+    const userId = req.user._id;
+    let updatedUser;
+    if(!profilePic){
+    updatedUser =   await User.findByIdAndUpdate(userId, {bio, fullName}, {new: true})
+      
+    } else{
+      const upload = await cloudinary.uploader.upload(profilePic);
+      updatedUser = await User.findByIdAndUpdate(userId, {profilePic: upload, secure_url, bio, fullName}, {new: true});
+    }
+    res.json({success:true, user: updatedUser})
+  } catch (error) {
+    console.log(error.message);
+    res.json({success: false, message: error.message})
+    
+  }
 }
